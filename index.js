36f14aff24d196d01df5a4d30da6955d619e3331
Shivraj <shivraj@geekyants.com>
2017-07-26 16:25:19 +0530

    added expo and yarn

--- index.js
+++ index.js
@@ -3,22 +3,30 @@
 
 var fs = require("fs-extra");
 var cmd = process.argv[2];
+var cmd2 = process.argv[3];
 var init = require("./modules/init");
+var initExpo = require("./modules/initExpo");
 
 var renameProject = function() {
-  var oldName = process.argv[3];
-  var newName = process.argv[4];
-  require("./modules/renameProject.js")(oldName, newName);
+	var oldName = process.argv[3];
+	var newName = process.argv[4];
+	require("./modules/renameProject.js")(oldName, newName);
 };
 
 var displayHelp = function() {
-  console.log("This is a help message");
+	console.log("This is a help message");
 };
 
 switch (cmd) {
-  case "init":
-    init();
-    break;
-  default:
-    displayHelp();
+	case "init":
+		switch (cmd2) {
+			case "--expo":
+				initExpo();
+				break;
+			default:
+				init();
+		}
+		break;
+	default:
+		displayHelp();
 }
--- modules/initExpo.js
+++ modules/initExpo.js
@@ -1,11 +1,14 @@
 var spawnSync = require("child_process").spawnSync;
-var execSync = require("child_process").execSync;
 var path = require("path");
 
-module.exports = function init() {
-	var initProcess = spawnSync("git", ["clone", "git@github.com:GeekyAnts/native-base-boilerplate.git"], {
-		stdio: "inherit",
-	});
+module.exports = function initExpo() {
+	var initProcess = spawnSync(
+		"git",
+		["clone", "-b", "Expo", "git@github.com:GeekyAnts/native-base-boilerplate.git"],
+		{
+			stdio: "inherit",
+		}
+	);
 
 	function getYarnVersionIfAvailable() {
 		var yarnVersion;
